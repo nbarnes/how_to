b70da84d52ac81a6b2d9bb9385f3f7879d33711e
NBarnes <nbarnes@gmail.com>
2013-10-23 18:31:54 -0700

    Initial commit of clean rails new how_to, added README.md

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HowTo::Application.config.secret_token = 'fcf3212e078ee0ea0e5bc698d5d2a97e9b4fa34524e014eb846e22466fa6050a70ef0d88f44c8e3cdf8dda27e1d372c0886eca06888dd520daf9facf12db36a4'
